1383563fd0e7e3638ed88362e9054c4d9f9f7bc4
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-12-15 22:19:42 +0000

    Initial commit

---
+++ btree.rb
@@ -0,0 +1,26 @@
+require_relative 'node.rb'
+
+class BinaryTree
+  def initialize
+    @root = Node.new
+  end
+
+  def build_tree(list)
+  end
+
+  def print_tree
+    # Level order traversal
+    # Preorder traversal
+    # Inorder traversal
+    # Postorder traversal
+  end
+
+  def breadth_first_search(item)
+  end
+
+  def depth_first_search(item)
+  end
+
+  def depth_first_search_rec(item)
+  end
+end
